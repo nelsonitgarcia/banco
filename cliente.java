04e6cd7c9e242fb66e6cf78e6941ea416997d44f
nelsonitgarcia <nelsonmgarcia@gmail.com>
2017-07-22 21:43:54 -0600

    first commit

---
+++ cliente.java
@@ -0,0 +1,33 @@
+public class Cliente{
+    private Integer idCliente;
+    private String nombreCliente;
+    private Intger nit;
+    
+    public void cliente(){
+    }
+    
+    public void setIdCliente(Integer aIdCliente){
+        this.idCliente = aIdCliente;
+    }
+    
+    public Integer getIdCliente(){
+        return this.idCliente;
+    }
+    
+    public void setNombreCliente(String aNombreCliente) {
+        this.nombreCliente = aNombreCliente;         
+    }
+        
+    public void getNombreCliente() {
+        return this.nombreCliente;         
+    }
+    
+    public void setNit(Integer aNit){
+        this.nit = aNit;
+    }
+    
+    public Integer getNit(){
+        return this.nit;
+    }        
+
+}
